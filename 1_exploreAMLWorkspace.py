e64817fa1c530ddadcc672ebec57acd5ca7b7c32
PortiaMasibi <portia.masibi@mail.mcgill.ca>
2025-09-17 20:03:06 -0400

    certification topics

---
+++ 1_exploreAMLWorkspace.py
@@ -0,0 +1,14 @@
+# EXPLORE AND CONFIGURE THE AZURE MACHINE LEARNING WORKSPACE
+
+# Using Azure Machine Learning Python SDK
+from azure.ai.ml.entities import Workspace
+
+workspace_name = "mlw-example"
+
+ws_basic = Workspace(
+    name=workspace_name,
+    location="eastus",
+    display_name="Basic workspace-example",
+    description="This example shows how to create a basic workspace",
+)
+ml_client.workspaces.begin_create(ws_basic)
